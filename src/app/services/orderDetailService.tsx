d30c98244e0e3d49badc5e0c8dfc27d1d694fa23
Thanh Huynh <thanhhuynh1202@gmail.com>
2023-12-14 13:18:51 +0700

    thong ke

--- src/app/services/orderDetailService.tsx
+++ src/app/services/orderDetailService.tsx
@@ -9,3 +9,19 @@ export const findAllByOrderId = async (orderId: any) => {
   );
   return response;
 };
+
+export const findOrderDetailByDate = async (
+  from: any,
+  to: any
+) => {
+  const response = await axiosClient.get(
+    `http://localhost:8080/api/v1/orders/findbydate`,
+    {
+      params: {
+        from: from, // adjust the parameter name based on your backend API
+        to: to,
+      },
+    }
+  );
+  return response;
+};
