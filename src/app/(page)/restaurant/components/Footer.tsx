ab956cab320e1ce71d29c2b206a00bac66560bf2
RinLeNe <145673439+RinLeNe@users.noreply.github.com>
2023-12-20 06:37:51 +0700

    finish

--- src/app/(page)/restaurant/components/Footer.tsx
+++ src/app/(page)/restaurant/components/Footer.tsx
@@ -455,6 +455,7 @@ const Footer: React.FC<IProps> = (props) => {
                   danger
                   block
                   onClick={showPromiseConfirm}
+                  //@ts-ignore
                   disabled={props.room?.roomOrders?.length > 0 ? false : true}
                 >
                   Thanh toán
