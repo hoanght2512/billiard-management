d991f921fbc4f43a1bc8e20771a06d165ee4c054
Thanh Huynh <thanhhuynh1202@gmail.com>
2023-12-19 15:46:03 +0700

    19/12

--- src/app/services/customerService.tsx
+++ src/app/services/customerService.tsx
@@ -12,14 +12,14 @@ interface customerPayload {
     return response;
   }
   export const addCustomer = async (customerDetail: customerPayload) => {
-    const response = await axiosClient.post("http://localhost:8080/api/v1/customer/manager", JSON.stringify(customerDetail));
+    const response = await axiosClient.post("http://localhost:8080/api/v1/customer/cashier", JSON.stringify(customerDetail));
     return response;
   }
   export const deleteCustomer = async (deleteId: any) => {
-    const response = await axiosClient.delete(`http://localhost:8080/api/v1/customer/manager/${deleteId}`)
+    const response = await axiosClient.delete(`http://localhost:8080/api/v1/customer/cashier/${deleteId}`)
     return response;
   }
   export const updateCustomer = async (updId:any, customerDetail: customerPayload) => {
-    const response = await axiosClient.put(`http://localhost:8080/api/v1/customer/manager/${updId}`, JSON.stringify(customerDetail));
+    const response = await axiosClient.put(`http://localhost:8080/api/v1/customer/cashier/${updId}`, JSON.stringify(customerDetail));
     return response;
   }
